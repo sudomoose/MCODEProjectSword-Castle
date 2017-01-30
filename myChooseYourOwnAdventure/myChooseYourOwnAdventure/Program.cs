621a0ae0cd7cc23ddbf6f1988c96e224693368e6
hilmakh <hilmakh@hotmail.com>
2017-01-29 21:19:39 -0800

    completed 2nd option
    activity type

--- myChooseYourOwnAdventure/myChooseYourOwnAdventure/Program.cs
+++ myChooseYourOwnAdventure/myChooseYourOwnAdventure/Program.cs
@@ -7,7 +7,7 @@ namespace myChooseYourOwnAdventure
     {
         static void Main(string[] args)
         {
-           Console.WriteLine("hello world!");
+            Console.WriteLine("hello world!");
             Console.WriteLine("Press any key to start!");
             Console.ReadKey();
             Console.WriteLine("Welcome to the best game on planet e-arrth! Please type your name!");
@@ -23,11 +23,13 @@ namespace myChooseYourOwnAdventure
             {
                 warriorType = "archer";
                 warriorWeapon = "bow and arrow";
-            } else if(warriorInput.KeyChar == '2')
+            }
+            else if (warriorInput.KeyChar == '2')
             {
                 warriorType = "swordsman";
                 warriorWeapon = "sword";
-            } else
+            }
+            else
             {
                 Console.Clear();
                 Console.WriteLine("please select a valid option");
@@ -38,7 +40,30 @@ namespace myChooseYourOwnAdventure
             Console.ReadKey();
             Console.WriteLine("Here is your " + warriorWeapon);
             Console.ReadKey();
-            Console.WriteLine("Would you like to practice with your " + warriorWeapon + " (1) or get to know people in town (2)"); 
+            
+        SelectActivity:
+            Console.WriteLine("Would you like to practice with your " + warriorWeapon + " (1) or get to know people in town (2)");
+            ConsoleKeyInfo activityInput = Console.ReadKey();
+            var activityType = "";
+            var activityTypeAction = "";
+
+            if (activityInput.KeyChar == '1')
+            {
+                activityType = "practice";
+                activityTypeAction = "You grab your " + warriorWeapon + "and begin to practice.";
+            }
+            else if (activityInput.KeyChar == '2')
+            {
+                activityType = "not practice";
+                activityTypeAction = "You begin to walk around.";
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("please select a valid option");
+                goto SelectActivity;
+            }
+            Console.WriteLine(activityTypeAction);
         }
     }
 }
